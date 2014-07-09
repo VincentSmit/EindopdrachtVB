00ec1c4cc006be9f53f813400deb3d6034758e9e
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2014-07-09 12:14:32 +0200

    Compile javadoc to docs/ directory

--- src/javadoc.sh
+++ src/javadoc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-javadoc -tag requires:cm:"Requires:" -tag ensures:cm:"Ensures:" -classpath ~/code/vb2/tests/build/antlr-3.5.2-complete.jar:. ast checker reporter symtab generator
+javadoc -tag requires:cm:"Requires:" -tag ensures:cm:"Ensures:" -classpath ~/code/vb2/tests/build/antlr-3.5.2-complete.jar:. ast checker reporter symtab generator -d ../docs
